62e1eeef8f54febac312552cab9e30e51045a8b3
dongjun6343 <dongjun6343@naver.com>
2024-07-12 17:31:09 +0900

    :sparkles: enum 활용

---
+++ src/mid1/enumeration/auth/AuthGrade.java
@@ -0,0 +1,26 @@
+package mid1.enumeration.auth;
+
+/**
+ * 인증 등급 열거형 활용하기
+ */
+public enum AuthGrade {
+    GUEST(1, "손님"),
+    LOGIN(2, "로그인 회원"),
+    ADMIN(3, "관리자");
+
+    private final int level;
+    private final String description;
+
+    AuthGrade(int level, String description) {
+        this.level = level;
+        this.description = description;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
